49029f2db7b86611a107e8c574438942bf9f3065
Hannah Liu <liuruohannah@gmail.com>
2016-08-24 06:36:48 +0000

    tab function complet

---
+++ script/design.js
@@ -0,0 +1,32 @@
+/* global $ */
+
+function main(){
+  
+  console.log("in main function");
+  // display color tab
+  displayTab("color");
+  //register click function
+  
+  $(".tab").click(function(){
+    console.log("click event catched and in function");
+    var tabName = $(this).attr("point-to");
+    displayTab(tabName);
+  })
+}
+
+
+function displayTab(tabName){
+  $(".inner-content").hide();
+  $(".tab").removeClass("active");
+  
+  $("[point-to ="+tabName+"]").addClass("active");
+  $("#"+tabName).fadeIn(500);
+  if ($(window).scrollTop() > 0) {
+     $('body,html').animate({
+        scrollTop : 0                 
+    }, 500);
+  }
+}
+
+$(document).ready(main());
+
