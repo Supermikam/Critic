47021d5b86b57df53f14af2603d0f5d3df7ce1d0
Hannah Liu <liuruohannah@gmail.com>
2016-08-31 10:21:17 +0000

    finish navigation page

--- script/nav.js
+++ script/nav.js
@@ -1,16 +1,11 @@
 /* global $ */
 function main(){
-  $('#homepage-link').mouseleave(function(){
-    console.log("focusout event triggered")
-    $(this).animate({width:30, height:30},200)
-  });
-  
-  $('#homepage-link').click(function(){
-  console.log("in expandDiv function")
-  $(this).animate(
-    { width: 500, height: 500}, 200
-    )
-  })
+  $('inner-content').hide();
+  $('.explain').hover(function(){
+  $(this).children('.inner-content').delay(500).show(200);
+  $(this).toggleClass('blink');}, 
+  function(){$(this).children('.inner-content').delay(500).hide(200);
+  $(this).toggleClass('blink');})
 }
 
 $(document).ready(main());
